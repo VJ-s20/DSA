df22916c69c4aa34c68a44c2677bc6b72eaaccbe
vijay <sahanivijay800@gmail.com>
2021-01-12 00:28:04 +0530

    Time complexity -O(n^2)

--- Data_structure/Array/Newyear_chaos.py
+++ Data_structure/Array/Newyear_chaos.py
@@ -7,17 +7,16 @@ queue=[1,2, 5, 3, 7, 8, 6, 4]
 queue=[2,1,5,3,4]
 
 
-def print_helper(intial,final,name):
-    print(f"{name}: Moved to {final} to {intial}")
+
 
 def minimumBribes(q):
     birbes=0
-    for i in range(len(q)-1):
-        val=q[i]-i
-        if val>3:
+    for i in range(len(q)):
+        if q[i]-i>3:
             return "TOO chaotic"
-        for j in range(i+1,len(q)):
-            birbes+=q[j]-i
+        for j in range(max(0,q[i]-2),i):
+            if q[j]>q[i]:
+                birbes+=1
         
     return birbes
 print(minimumBribes(queue))
